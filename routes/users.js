5399caffd72fb69d9dffd7f610fbb8410aacc14b
Binyameen <bmghafoor@gmail.com>
2023-04-29 00:18:31 -0400

    Returns user with jobs they applied for

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,11 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn, ensureIsAdmin, ensureisAdminOrUser } = require("../middleware/auth");
+const {
+  ensureLoggedIn,
+  ensureIsAdmin,
+  ensureisAdminOrUser,
+} = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -13,7 +17,6 @@ const userNewSchema = require("../schemas/userNew.json");
 const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
-
 
 /** POST / { user }  => { user, token }
  *
@@ -31,7 +34,7 @@ router.post("/", ensureIsAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -42,7 +45,6 @@ router.post("/", ensureIsAdmin, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 /** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
  *
@@ -60,7 +62,6 @@ router.get("/", ensureIsAdmin, async function (req, res, next) {
   }
 });
 
-
 /** GET /[username] => { user }
  *
  * Returns { username, firstName, lastName, isAdmin }
@@ -77,7 +78,6 @@ router.get("/:username", ensureisAdminOrUser, async function (req, res, next) {
   }
 });
 
-
 /** PATCH /[username] { user } => { user }
  *
  * Data can include:
@@ -88,35 +88,55 @@ router.get("/:username", ensureisAdminOrUser, async function (req, res, next) {
  * Authorization required: login
  **/
 
-router.patch("/:username", ensureisAdminOrUser, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, userUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
+router.patch(
+  "/:username",
+  ensureisAdminOrUser,
+  async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map((e) => e.stack);
+        throw new BadRequestError(errs);
+      }
+
+      const user = await User.update(req.params.username, req.body);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
     }
-
-    const user = await User.update(req.params.username, req.body);
-    return res.json({ user });
-  } catch (err) {
-    return next(err);
   }
-});
-
+);
 
 /** DELETE /[username]  =>  { deleted: username }
  *
  * Authorization required: login
  **/
 
-router.delete("/:username", ensureisAdminOrUser, async function (req, res, next) {
-  try {
-    await User.remove(req.params.username);
-    return res.json({ deleted: req.params.username });
-  } catch (err) {
-    return next(err);
+router.delete(
+  "/:username",
+  ensureisAdminOrUser,
+  async function (req, res, next) {
+    try {
+      await User.remove(req.params.username);
+      return res.json({ deleted: req.params.username });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
+router.post(
+  "/:username/jobs/:id",
+  ensureisAdminOrUser,
+  async (req, res, next) => {
+    try {
+      const { username, id } = req.params;
+      await User.applyForJob(username, id);
+      return res.json({ msg: `${username} applied for job with id ${id}` });
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
 
 module.exports = router;
